7af1c99bf5036af7b16b54bd7bdeb47fbf39a691
Bermuda2001 <69179728+Bermuda2001@users.noreply.github.com>
2021-11-04 09:50:47 +0700

    Add files via upload

---
+++ type of survier/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1635993422,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/street-sheet0.png",
+		"images/stop-sheet0.png",
+		"images/army-sheet0.png",
+		"images/army-sheet1.png",
+		"images/stand-sheet0.png",
+		"images/stand-sheet1.png",
+		"images/stand-sheet2.png",
+		"images/stand-sheet3.png",
+		"images/spritefont.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"words1.xml",
+		"words.txt"
+	]
+}
